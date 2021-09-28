106ebd2ab0e8c38377edd9f88667c75ce834380a
Marek_p <marek.mnp@seznam.cz>
2021-09-28 13:05:45 +0200

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -5,16 +5,16 @@ class Serv(BaseHTTPRequestHandler):
 
     def do_GET(self):
         if self.path == '/':
-            self.path = '/index.html'
+            self.path = '/index.html' # Spouštěcí soubor
         try:
             file_to_open = open(self.path[1:]).read()
             self.send_response(200)
         except:
-            file_to_open = '[Error] Soubor nenalezen!'
+            file_to_open = '[Error] Soubor nenalezen!' # Error zpráva
             self.send_response(404)
         self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
+        self.wfile.write(bytes(file_to_open, 'utf-8')) # Encoding
 
 
-httpd = HTTPServer(('localhost', 8080), Serv) 
+httpd = HTTPServer(('localhost', 8080), Serv) # Hostname + Port
 httpd.serve_forever()
